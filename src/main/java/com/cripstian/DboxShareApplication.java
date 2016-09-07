fe17e4d47e2fe4a2fd0546c53e0f200168a94eaf
crip.popescu@gmail.com <28.Februarie>
2016-09-07 23:10:28 +0300

    First commit

---
+++ src/main/java/com/cripstian/DboxShareApplication.java
@@ -0,0 +1,12 @@
+package com.cripstian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DboxShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DboxShareApplication.class, args);
+	}
+}
